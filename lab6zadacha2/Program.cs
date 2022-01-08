e97bc55a3e3e4ee9b834b491cb4377b4f738a24c
vvpyankov@mail.ru <vvpyankov@mail.ru>
2022-01-08 11:40:30 +0300

    08.01.2022

---
+++ lab6zadacha2/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab6zadacha2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Console.Write("Введите предложение, слова разделены пробелами: ");
+            string vvod = Console.ReadLine();
+            string[] str = vvod.Split(new Char[] { ' ' });
+            int maxSlo = 0, index = 0;
+            for (int i = 0; i < str.Length; i++)
+            {
+                if (str[i].Length > maxSlo)
+                {
+                    maxSlo = str[i].Length;
+                    index = i;
+                }
+            }
+            Console.Write("Самое длинное слово: {0}", str[index]);
+            Console.ReadLine();*/
+
+            Console.WriteLine("Введите предложение, слова разделены пробелами: ");
+            string sentence = Console.ReadLine();
+            sentence = sentence.Replace(" ", "").ToLower();
+            Console.WriteLine(sentence);
+            string reverse = new string(sentence.ToCharArray().Reverse().ToArray());
+            if (sentence == reverse)
+                Console.WriteLine("Это предложение является палидромом");
+            if (sentence != reverse)
+                Console.WriteLine("Это предложение - не палидром... Начинайте заново.");
+            Console.ReadKey();
+        }
+    }
+}
